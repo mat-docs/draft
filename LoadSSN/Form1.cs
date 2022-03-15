e7304d00cab793f11584162d32ee0bde6a06b7bc
Wei Li <wei.li@mclaren.com>
2022-03-15 15:16:44 +0000

    re-order

---
+++ LoadSSN/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+using MAT.Atlas.Automation.Client.Services;
+
+namespace LoadSSN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var application = new ApplicationServiceClient();
+                    var sets = WorkbookServiceClient.Call(client => client.GetSets());
+                    if (sets.Length == 0)
+                    {
+                        throw new Exception("Ensure ATLAS is running...");
+                    }
+
+                    application.LoadFileSessions(sets[0].Id, openFileDialog.FileNames);
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.Message);
+                }
+            }
+        }
+    }
+}
